1948a985209629a627bae57dac96cf1ca38f5f11
curiousxeon <curiousxeon@outlook.com>
2026-04-15 09:52:42 +0800

    feat(rpc): 新增 rpc 核心接口与类型

---
+++ rpc/rpc_test.go
@@ -0,0 +1,14 @@
+package rpc_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/CXeon/tiles/rpc"
+)
+
+func TestResponseError_Error(t *testing.T) {
+	err := &rpc.ResponseError{Code: 1001, Message: "not found", TraceID: "abc-123"}
+	assert.Equal(t, "rpc error 1001: not found (trace_id=abc-123)", err.Error())
+}
